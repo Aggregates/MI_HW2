d74f400a6b4a9e87728028903f37dac7e9b09e5b
Tyler Haigh <tylerhaigh1994@me.com>
2015-05-30 20:21:07 +1000

    Add Monte Carlo AI Shell class

---
+++ src/main/python/hw2_65536/ai/montecarlo.py
@@ -0,0 +1,17 @@
+import random
+
+class MonteCarlo(object):
+
+    def __init__():
+
+    def nextMove():
+        '''
+        Decide which direction to move in.
+        Chosen direction will be between 1 and 4 (inclusive)
+        '''
+
+        direction = random.randint(1,4)
+        return direction
+
+if __name__ == "__main__":
+    print nextMove()
