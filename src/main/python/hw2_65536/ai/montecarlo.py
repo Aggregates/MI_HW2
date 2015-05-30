228955a53abc5c490f4a65ee065bd48c2e20484e
Tyler Haigh <tylerhaigh1994@me.com>
2015-05-30 20:57:17 +1000

    Expand work on MC shell

--- src/main/python/hw2_65536/ai/montecarlo.py
+++ src/main/python/hw2_65536/ai/montecarlo.py
@@ -2,7 +2,14 @@ import random
 
 class MonteCarlo(object):
 
-    def nextMove():
+    def __init__(self, target, stepSize):
+        self.state = 0
+        self.targetValue = target
+        self.stepSize = stepSize
+        self.stateValue = 0
+
+
+    def nextMove(self):
         '''
         Decide which direction to move in.
         Chosen direction will be between 1 and 4 (inclusive)
@@ -10,6 +17,13 @@ class MonteCarlo(object):
 
         direction = random.randint(1,4)
         return direction
+
+    def temporalDifference(self):
+        stateValue = self.predictValue(state) + stepSize * (self.targetValue - self.predictValue(state))
+
+
+    def predictValue(self, state, time):
+        return 1
 
 if __name__ == "__main__":
     print nextMove()
